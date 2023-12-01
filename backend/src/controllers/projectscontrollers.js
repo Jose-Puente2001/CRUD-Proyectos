0fdc36f1ac83005dc63dcbd9d30dfc2790e17eb5
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-12-01 11:28:34 -0400

    terminado del crud

--- backend/src/controllers/projectscontrollers.js
+++ backend/src/controllers/projectscontrollers.js
@@ -7,11 +7,17 @@ export const getProjects = async (req, res) => {
 
 }
 
+export const getProjectById = async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await connection.query("SELECT * FROM project WHERE id = ?", [id]);
+  res.json(rows[0]);
+};
+
 export const addProjects = async (req, res) => {
  
- const {cliente, nombre_proyecto} = req.body
- const [rows] = await connection.query("INSERT INTO project(cliente, nombre_proyecto) VALUES(?, ?)", 
- [cliente, nombre_proyecto]
+ const {cliente, nombre_proyecto, country} = req.body
+ const [rows] = await connection.query("INSERT INTO project(cliente, nombre_proyecto, country) VALUES(?, ?, ?)", 
+ [cliente, nombre_proyecto, country]
   );
 
 
@@ -20,8 +26,8 @@ export const addProjects = async (req, res) => {
 export const updateProjects = async (req, res) => {
 
 const {id} = req.params
-const {cliente, nombre_proyecto} = req.body
-const [result] = await connection.query("UPDATE project SET cliente = ?, nombre_proyecto = ? WHERE id = ?", [cliente, nombre_proyecto, id])
+const {cliente, nombre_proyecto, country, estatus} = req.body
+const [result] = await connection.query("UPDATE project SET cliente = ?, nombre_proyecto = ?, country = ?, estatus = ? WHERE id = ?", [cliente, nombre_proyecto, country, estatus, id])
 }
 
 export const deleteProjects = async (req, res) => {
--- backend/src/routes/projectsroutes.js
+++ backend/src/routes/projectsroutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { getProjects, addProjects, updateProjects, deleteProjects } from '../controllers/projectscontrollers.js'
+import { getProjects, addProjects, updateProjects, deleteProjects, getProjectById } from '../controllers/projectscontrollers.js'
 
 
 const router = Router()
 
 router.get('/proyectos', getProjects)
+router.get('/proyectos/:id', getProjectById)
 router.post('/agregarproyectos', addProjects)
 router.put('/actualizarproyectos/:id', updateProjects)
 router.delete('/eliminarproyectos/:id', deleteProjects)
